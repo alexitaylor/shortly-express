0459a901d3801e580e8e2ad77686a1ea33f9180a
Hack Reactor Student <student@hackreactor.com>
2017-04-25 12:33:37 -0700

    Implemented Sessions Schema and passed test for Sessions Schema

--- test/ServerSpec.js
+++ test/ServerSpec.js
@@ -47,7 +47,7 @@ describe('', function() {
     /**************************************************************************************/
     /* TODO: If you create a new MySQL tables, add it to the tablenames collection below. */
     /**************************************************************************************/
-    var tablenames = ['links', 'clicks', 'users', 'sessions'];
+    var tablenames = ['links', 'clicks', 'sessions', 'users'];
 
     db.connect(function(err) {
       if (err) { return done(err); }
@@ -281,7 +281,7 @@ describe('', function() {
     });
   });
 
-  xdescribe('Sessions Schema:', function() {
+  describe('Sessions Schema:', function() {
     it('contains a sessions table', function(done) {
       var queryString = 'SELECT * FROM sessions';
       db.query(queryString, function(err, results) {
@@ -330,7 +330,7 @@ describe('', function() {
     });
   });
 
-  xdescribe('Express Middleware', function() {
+  describe('Express Middleware', function() {
     var cookieParser = require('../server/middleware/cookieParser.js');
     var createSession = require('../server/middleware/auth.js').createSession;
 
